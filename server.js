cf9ddb874c16ad6d4c1f353d66ea4867d443203f
yujmo <yujmo94@gmail.com>
2017-07-17 10:59:38 +0800

    modify some files

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
 var http = require("http")
+function start(){
+    function onRequest(request,response){
+	console.log("Request received.");
+	response.writeHead(200,{"Content-Type":"text/plain"});
+	response.write("Hello World");
+	response.end();
+    }
+
+    http.createServer(onRequest).listen(8888);
+
+    console.log("Server has started.");
+}
+
+exports.start=start;
+
+
+
+
+
+
+/*
+=====================================================================
 http.createServer(function(request,response){
     response.writeHead(200,{"Content-Type":"text/plain"});
     response.write("hello world");
     response.end();
 }).listen(8888);
+=====================================================================
+*/
