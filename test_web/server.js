354f6bbc8cc830103cdc0e31f92f5fdc6eb7adfc
yujmo <yujmo94@gmail.com>
2017-07-17 15:39:15 +0800

    update some files

--- test_web/server.js
+++ test_web/server.js
@@ -1,6 +1,6 @@
 var http = require("http")
 var url = require("url")
-function start(route){
+function start(route,handle){
     function onRequest(request,response){
 	var pathname = url.parse(request.url).pathname;
 	console.log("Request for " + pathname + " received.");
