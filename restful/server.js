067f42451041a9a573f5cbd09ef23c625e5ca96c
yujmo <yujmo94@gmail.com>
2017-07-20 18:23:12 +0800

    modify some files

--- restful/server.js
+++ restful/server.js
@@ -20,6 +20,15 @@ app.get('/addUser',function(req,res){
 	});
 })
 
+app.get('/deleteUser',function(req,res){
+    fs.readFile(__dirname + "/" + "users.json",'utf8',function(err,data){
+	data = JSON.parse(data);
+	delete data["user" + 2];
+	console.log(data);
+	res.end(JSON.stringify(data));
+    });
+})
+
 function onRequest(req,res){
     fs.readFile(__dirname + "/" + "users.json",'utf8',function(err,data){
 	console.log(data);
@@ -28,6 +37,17 @@ function onRequest(req,res){
 }
 
 app.get('/listUsers',onRequest);
+
+app.get('/:id',function(req,res){
+    fs.readFile(__dirname + "/" + "users.json",'utf8',function(err,data){
+	data = JSON.parse(data);
+	var user = data["user" + req.params.id];
+	console.log(user);
+	res.end(JSON.stringify(user));
+    });
+})
+
+
 
 var server = app.listen(8888,function(){
     console.log("Server has started. ");
