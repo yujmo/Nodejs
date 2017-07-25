2a19b13719d70d5933a4d689fbf8bd3aa558dc5c
yujmo <yujmo94@gmail.com>
2017-07-25 18:26:09 +0800

    modify some files

--- kong/client_page/server.js
+++ kong/client_page/server.js
@@ -1,24 +1,11 @@
 var http = require('http');
-var request = require('request');
-/*
-http.createServer(function (request, response) {
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.end('Hello World\n');
+var url = require('url');
+
+http.createServer(function(request,response){
+    var pathname = url.parse(request.url).pathname; 
+    response.writeHead(200,{'Content-Type':'text/html'});
+    
+    response.end();
 }).listen(8887);
-console.log('Server running at http://127.0.0.1:8888/');
-*/
 
-var options={
-	hostname: "http://localhost",
-	port: 8000,
-	path: '/2',
-	method: 'GET'
-}
-
-
-var req=http.request(options,function(res){
-	console.log(res);
-
-})
-
-
+console.log("Server is running at http://192.168.171.130:8887");
