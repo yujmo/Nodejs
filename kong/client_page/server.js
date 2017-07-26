608fe57f6f48dfaa1d4703648b14425eb0ce9433
yujmo <yujmo94@gmail.com>
2017-07-26 10:58:49 +0800

    modify some files

--- kong/client_page/server.js
+++ kong/client_page/server.js
@@ -1,2 +1,20 @@
-a:wq
+var http = require('http');
+var options = {
+	hostname: '127.0.0.1',
+	port: '8000',
+	path: '/listusers',
+	method: 'GET',
+	headers: {
+		'Host': 'example.com',
+		'apikey': 'ENTER_KEY_HERE'
+	}
+}
 
+var req = http.request(options,function(res){
+	res.setEncoding('utf-8');
+	res.on('data',function(chunk){
+		console.log(chunk);
+	});
+	}
+);
+req.end()
