bf4fed0b7029ec07aa1c3052ac8945ed14e2c5bb
yujmo <yujmo94@gmail.com>
2017-07-27 18:03:22 +0800

    modify some files

--- kong/client_page/handle.js
+++ kong/client_page/handle.js
@@ -4,28 +4,29 @@ var querystring = require('querystring');
 function handle(postData,response,request,postHTML){
     response.writeHead(200,{"Content-type":"text/html"});
     response.write(postHTML);
+    cache();
     function cache(){
-	var option={
-		hostname: '127.0.0.1',
-		port: '8000',
-		path: querystring.parse(postData)["name"],
-		method: 'GET',
-		headers: {
-    			'Host': 'example.com',
-    			'apikey': 'ENTER_KEY_HERE'
-		}
-	};
-	temp = ""; 
 	
+	var options={
+	    hostname: '127.0.0.1',
+	    port: '8000',
+	    path: querystring.parse(postData)["name"],
+	    method: 'GET',
+	    headers: {
+		'Host': 'example.com',
+		'apikey': 'ENTER_KEY_HERE'
+	    }
+	};
+
 	var req = http.request(options,function(res){
-		res.setEncoding('utf8');
-		res.on('data',function(chunk){
-			temp = chunk;
-		});
+	    res.setEncoding('utf8');
+	    res.on('data',function(chunk){
+		console.log(chunk);
+	    });
 	});
-	req.end()
+    
+    req.end()
     }
-    response.write(cache);
     response.end();
 }
 exports.handle = handle;
